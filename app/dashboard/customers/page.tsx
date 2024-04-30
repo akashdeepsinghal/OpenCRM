de37540a14f96e1084353814b781518461bed39a
Akash Deep Singhal <akigupta131@gmail.com>
2024-04-30 18:07:14 +0530

    Update customer page and add customer creation button

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,8 @@
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import CustomersTable from '@/app/ui/customers/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { inter, lusitana } from '@/app/ui/fonts';
+import { CreateCustomer } from '@/app/ui/customers/buttons';
+import { inter } from '@/app/ui/fonts';
 import { CustomersTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchCustomersPages, fetchFilteredCustomers } from '@/app/lib/data';
@@ -34,7 +34,7 @@ export default async function Page({
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
-        <CreateInvoice />
+        <CreateCustomer />
       </div>
       <Suspense key={query + currentPage} fallback={<CustomersTableSkeleton />}>
         <CustomersTable query={query} currentPage={currentPage} />
